72eca9ecee241263c583c0051df79c9fdd2f8963
winmoh <mohamedaouinti2023@gmail.com>
2023-12-22 00:49:06 +0100

    adding AST nodes  valdators 1

--- src/language/sboot-validator.ts
+++ src/language/sboot-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { SbootAstType, Person } from './generated/ast.js';
+import { ValidationCategory, type ValidationAcceptor, type ValidationChecks } from 'langium';
+import { type SbootAstType, type Person, Controller, Service, DTO } from './generated/ast.js';
 import type { SbootServices } from './sboot-module.js';
 
 /**
@@ -9,7 +9,17 @@ export function registerValidationChecks(services: SbootServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.SbootValidator;
     const checks: ValidationChecks<SbootAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        Entity: validator.checkEntityAttributes,
+        Controller:  validator.checkController,
+        Service: validator.checkService,
+        DTO: validator.CheckDto,
+        ControllerAction: validator.checkConfiguration,
+        ServerInfo: validator.checkServiceInfo,
+        DatabaseInfo: validator.checkDatabaseInfo
+        
+        
+
+
     };
     registry.register(checks, validator);
 }
@@ -18,14 +28,27 @@ export function registerValidationChecks(services: SbootServices) {
  * Implementation of custom validations.
  */
 export class SbootValidator {
+    checkDatabaseInfo(){}
+    checkServiceInfo(){}
+    checkConfiguration(){
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    }
+    checkController(){}
+    checkService(){
+        
+    }
+    CheckDto(){}
+    checkEntityAttributes(){
+
+    }
+
+    /*checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
         if (person.name) {
             const firstChar = person.name.substring(0, 1);
             if (firstChar.toUpperCase() !== firstChar) {
                 accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
             }
         }
-    }
+    }*/
 
 }
